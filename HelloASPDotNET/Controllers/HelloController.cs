925ac340d2b1eeec5a842852efb5ccca1d5b9f43
Abby Howe <howe.abigail@gmail.com>
2020-11-20 17:04:54 -0600

    Initial Commit - Query Parameters Lesson

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        [Route("/helloworld/")]
+        public IActionResult Index()
+        {
+            string html = "<h1>Hello World!</h1>";
+            return Content(html, "text/html");
+        }
+
+        [HttpGet]
+        [Route("/helloworld/welcome/{name?}")]
+        public IActionResult Welcome(string name = "Abby")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
