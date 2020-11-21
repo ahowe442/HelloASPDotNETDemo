dde0870ff83a787125b3d67a34f2bfc26c17c08a
Abby Howe <howe.abigail@gmail.com>
2020-11-20 21:36:23 -0600

    adds wires up the View/Hello/Index to the Controller

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -8,25 +8,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HelloASPDotNET.Controllers
 {
-    [Route("/helloworld")]
+    
     public class HelloController : Controller
     {
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld/'>" +
-                "<input type='text' name='name' />" +
-                "<input type='submit' value='Greet Me!'/>" +
-                "</form>";
-             
-            return Content(html, "text/html");
+            return View();
         }
 
         //[HttpGet]
         //[Route("/helloworld/welcome/{name?}")]
 
         [HttpPost]
+        [Route("/hello")]
         public IActionResult Welcome(string name = "World")
         {
             return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
